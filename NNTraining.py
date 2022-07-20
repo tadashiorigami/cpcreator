28c9b40726957ab24a923141cf1d210d1483edf7
tadashimori <tadashimori.br@gmail.com>
2022-07-20 11:11:35 -0300

    update activation function

--- NNTraining.py
+++ NNTraining.py
@@ -16,9 +16,9 @@ setEpochs = 100
 weights = "firsttest.h5"
 
 model = Sequential([
-  Dense(64, activation='relu', input_shape=(arraySize,)),
-  Dense(64, activation='relu'),
-  Dense(64, activation='relu'),
+  Dense(64, activation='sigmoid', input_shape=(arraySize,)),
+  Dense(64, activation='sigmoid'),
+  Dense(64, activation='sigmoid'),
   Dense(2, activation='softmax'),
 ])
 
